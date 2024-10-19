c440decb95773279b3bbd71790f3b0d320deae0a
NikiosDev <Nikolaevaolga2002@mail.ru>
2024-10-19 16:44:57 +0300

    Sliders are ready

--- lightCorrector/ViewController.swift
+++ lightCorrector/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var valueforBlue: UILabel!
+    @IBOutlet weak var valueForRed: UILabel!
+    @IBOutlet weak var valueForGreen: UILabel!
+    
+    @IBOutlet weak var sliderForRed: UISlider!
+    @IBOutlet weak var sliderForBlue: UISlider!
+    @IBOutlet weak var sliderForGreen: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mainView.layer.cornerRadius = 10
+        
+        sliderForBlue.value = 0.5
+        sliderForBlue.minimumValue = 0
+        sliderForBlue.maximumValue = 1
+        
+        sliderForRed.value = 0.5
+        sliderForRed.minimumValue = 0
+        sliderForRed.maximumValue = 1
+        
+        sliderForGreen.value = 0.5
+        sliderForGreen.minimumValue = 0
+        sliderForGreen.maximumValue = 1
+    }
+    @IBAction func greenSliderAction() {
+        valueForGreen.text = String(format: "%.2f", sliderForGreen.value)
     }
 
-
+    @IBAction func redSliderAction() {
+        valueForRed.text = String(format: "%.2f", sliderForRed.value)
+    }
+    @IBAction func blueSliderAction() {
+        valueforBlue.text = String(format: "%.2f", sliderForBlue.value)
 }
-
+    
+}
